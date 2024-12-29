1033c2772da4a4f85841a8d62549c69217bc4b6d
Rushiraj <rushirajsinhherma7@gmail.com>
2024-12-29 14:11:45 -0500

    import change comic.jsx

--- react_client/src/components/App.jsx
+++ react_client/src/components/App.jsx
@@ -1,55 +1,10 @@
-// import React from 'react';
-// import './App.css';
-// import {NavLink, Route, Routes} from 'react-router-dom';
-// import { AppBar, Toolbar, Typography, Button, Box, TextField, IconButton } from '@mui/material';
-// import Home from './Home';
-// import Comics from './Comics';
-// import Comic from './comic';
-// import Collection from './Collections';
-// import Error_404 from './Error_404';
-// import Error_400 from './Error_400'
-// import Check_Error from './Check_Error';
-// function App() {
-
-//   return (
-//     <div>
-//       <AppBar position="static">
-//       <Toolbar sx={{ justifyContent: 'space-between' }}>
-//         <Button color="inherit" component={NavLink} to="/">
-//           Home
-//         </Button>
-//         <Typography variant="h6" component="div">
-//           MARVEL-COMICS COLLECTION
-//         </Typography>
-//         <Button color="inherit" component={NavLink} to="/marvel-comics/page/1">
-//           Explore Comics
-//         </Button>
-//       </Toolbar>
-//     </AppBar>
-
-//       <Routes>
-//         <Route path='/' element={<Collection />} />
-//         <Route path='/marvel-comics/page/:pagenum' element={<Comics />} />
-//         <Route path='/marvel-comics/:id' element={<Comic />} />
-//         <Route path='/marvel-comics/collections' element={<Collection />} />
-//         <Route path='/404_error' element = {<Error_404/>}/>
-//         <Route path='/400_error' element = {<Error_400/>}/>
-//         <Route path='/error' element = {<Check_Error/>}/>
-//       </Routes>
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
 import {NavLink, Route, Routes} from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import Home from './Home';
 import Comics from './Comics';
-import Comic from './comic';
+import Comic from './comic.jsx';
 import Collection from './Collections';
 import Error_404 from './Error_404';
 import Error_400 from './Error_400'
